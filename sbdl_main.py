3139cebd8fa1a9980482f6af89b3e31361540982
Saurabh Chaurasia <about.srb@gmail.com>
2024-01-19 21:20:31 +0530

    added module to read the csv data

--- sbdl_main.py
+++ sbdl_main.py
@@ -1,6 +1,8 @@
 import sys
+import uuid
+from pyspark.sql.functions import struct, col, to_json
 
-from lib import Utils
+from lib import ConfigLoader, Utils, DataLoader
 from lib.logger import Log4j
 
 if __name__ == '__main__':
@@ -11,8 +13,18 @@ if __name__ == '__main__':
 
     job_run_env = sys.argv[1].upper()
     load_date = sys.argv[2]
+    job_run_id = "SBDL-" + str(uuid.uuid4())
 
+    print("Initializing SBDL Job in " + job_run_env + " Job ID: " + job_run_id)
+    conf = ConfigLoader.get_config(job_run_env)
+    enable_hive = True if conf["enable.hive"] == "true" else False
+    hive_db = conf["hive.database"]
+
+    print("Creating Spark Session")
     spark = Utils.get_spark_session(job_run_env)
+
     logger = Log4j(spark)
+
+    accounts_df = DataLoader.read_accounts(spark, job_run_env, enable_hive, hive_db )
 
     logger.info("Finished creating Spark Session")
